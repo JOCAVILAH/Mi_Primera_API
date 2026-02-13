7ead61c34e5c498deafdde3a5b04cdba89cc8d8d
Jordan Camilo Avila <jordanherrera357@gmail.com>
2026-02-12 20:54:41 -0500

    v0.2

---
+++ Frontend/programa.js
@@ -0,0 +1,13 @@
+//Frontend mínimo
+// variable se crea con let--> creamos una variable url
+let url = "http://127.0.0.1:8000/sumar"
+// envia a la api parametros a y b
+let myAPI = url + "?a=5&b=45"
+
+// async funtion es un proceso asincronico.
+// se conecta a la api mediante fetch y guardo la respuesta en response, y await da la espera de respuesta
+// se guarda la respuesta en json em la variable datos
+async function crearPeticion(){
+let response = await fetch(myAPI)
+let datos = response.json()
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # proyecto demo básico de API.
+
+## PARA EL BACKEND
+
+1. Crear un ambiente virtual 
+phyton -m venv venv .myenv
+ python -m venv .myenv
+.\.myenv\Scripts\activate
+2. Instalar la biblioteca fastapi
